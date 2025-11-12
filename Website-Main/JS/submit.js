7b984ebe7e3473c2099f72aab57ce161d3dae289
zoey-snuffles <0zoeysheridan0@gmail.com>
2025-11-12 14:59:13 +0000

    11.11.25 zoey

--- Website-Main/JS/submit.js
+++ Website-Main/JS/submit.js
@@ -1,25 +1,7 @@
-/* problem for another day*/
-
-function validateForm() {
-      //Regexs for validation, match only letters.
-      var letters = /^[A-Za-z]+$/;
 
 
-      //get form 
-      var myForm = document.getElementById('frm1');
-      //get textboxes
-      var cob = myForm.elements[0].value;
-      var yob = myForm.elements[1].value;
-      //get select
-      var gndr = myForm.elements[2].value;
-
-      console.log(cob.value)
-      //validate the value of destination, Only letters allowed, no spaces, symbols or numbers.
-      if (!cob.match(letters)) {
-        alert('Please enter a valid destination');
-        return false;
-      }
-      myform.submit();
-    }
+function contactMessage(){
+  document.getElementById("formTable").innerHTML = "Submitted. We will be in contact shortly!";
+}
 
   
