086051aeb70be05dd60c39bbfb6c3415492a3367
zoey-snuffles <0zoeysheridan0@gmail.com>
2025-12-01 11:50:29 +0000

    Regular expressions and javascript

--- Website-Main/JS/submit.js
+++ Website-Main/JS/submit.js
@@ -1,6 +1,43 @@
 
+function validate() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const feedback = document.getElementById('picDiv');
+  
 
-function contactMessage(){
-  document.getElementById("formStyle").innerHTML = "Submitted. We will be in contact shortly!";
+  const namePattern = /^[a-zA-Z_ ]*$/;
+  const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+  const phonePattern = /^[0]+[0-9]{9}$/;
+  
+
+ 
+    
+
+  if (!namePattern.test(name)){
+    feedback.style = "display : block"
+    console.log("name")
+  } 
+  
+  else if (!emailPattern.test(email)){
+    feedback.style = "display : block"
+    console.log("email")
+  }
+   
+  else if (!phonePattern.test(phone)){
+    feedback.style = "display : block"
+    console.log("phone")
+  }
+
+  else{
+    document.getElementById("formStyle").innerHTML = "Submitted. We will be in contact shortly! <img src = \'../Media/ploarbearyes.png\' width = \'128\' height = \'128\'>"
+  }
+  
+  
 }
 
+
+
+
+
+ 
